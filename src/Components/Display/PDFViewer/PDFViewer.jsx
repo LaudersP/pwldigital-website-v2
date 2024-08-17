08628bbf8e5e6e68324c9b0e86cf244185e722bd
Parker Lauders <parkerlauders04@gmail.com>
2024-08-17 01:39:48 -0400

    feat: Added mobile support to the component

--- src/Components/Display/PDFViewer/PDFViewer.jsx
+++ src/Components/Display/PDFViewer/PDFViewer.jsx
@@ -1,33 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./PDFViewer.css";
 
 import Button from "../../Interaction/Button/Button";
 
 const PDFViewer = ({ label, filename, alt, title }) => {
+  const [isSupported, setIsSupported] = useState(true);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 600) {
+        setIsSupported(false);
+      } else {
+        setIsSupported(true);
+      }
+    };
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   const handleContextMenu = (event) => {
     event.preventDefault();
   };
 
   return (
-    <div className="pdfviewer-label">
+    <div className="pdfviewer-container">
       <div className="pdfviewer-label">{label}</div>
-      <div className="pdfviewer-pdf">
-        <img
-          src={filename}
-          alt={alt}
-          title={title}
-          onContextMenu={handleContextMenu}
-        />
+      <div className="pdfviewer-content">
+        {isSupported ? (
+          <img
+            src={filename}
+            alt={alt}
+            title={title}
+            onContextMenu={handleContextMenu}
+          />
+        ) : (
+          <div className="unsupported-message">
+            This device does not support the viewer, please download instead.
+          </div>
+        )}
       </div>
       <div className="download-button">
         <Button
           iconName="download"
           label="Download PDF"
           type="download"
-          destination="/files/Parker Lauders Computer Engineering Technology.pdf"
+          destination={filename}
         />
       </div>
     </div>
   );
 };
+
 export default PDFViewer;
