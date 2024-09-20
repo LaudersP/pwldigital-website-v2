32bd11e82df639adcbdd6b0600bd7210f3cbe788
Parker Lauders <parkerlauders04@gmail.com>
2024-09-20 14:07:32 -0400

    feat: Add more appearances

--- src/Pages/AppearancesPage/AppearancesPage.jsx
+++ src/Pages/AppearancesPage/AppearancesPage.jsx
@@ -8,7 +8,111 @@ function AppearancesPage() {
   return (
     <div className="socials-page">
       <PageWrapper>
-        <Section label="Athletic Articles" bottomBorder topOfPage>
+        <Section label="Video Appearances" bottomBorder topOfPage>
+          <ArticleSummary
+            src="/article-thumbnails/senate-grant.png"
+            articleTitle="Shawnee State University - A Hidden Gem on the Cutting Edge of Technology"
+            articleURL="https://youtu.be/xT96IT0iuNA?si=suDH1Snet0AiBvVd"
+            articleAuthor="Ohio Senate Republicans"
+            articleSite="YouTube"
+            video
+          >
+            Parker Lauders recently took part in a key meeting with Ohio
+            Senators, alongside fellow Shawnee State University graduates, to
+            advocate for a grant supporting a robotics lab. During the meeting,
+            Parker emphasized the importance of Shawnee State and shared
+            insights from his university research, focusing on how automation
+            and AI are transforming industrial manufacturing. This event
+            underscores Shawnee State's dedication to innovation and the vital
+            role its graduates play in advancing technology-driven industries.
+          </ArticleSummary>
+          <ArticleSummary
+            src="/article-thumbnails/innovation-award-2024.png"
+            articleTitle="Presidential Awards for Innovation 2024"
+            articleURL="https://youtu.be/DRW-u3qW4zw?si=TO7J33pXDqjNPFs2"
+            articleAuthor="Shawnee State University"
+            articleSite="YouTube"
+            video
+          >
+            During the installation of Dr. Eric Andrew Braun as Shawnee State
+            University's eighth president, Parker Lauders received the
+            prestigious Presidential Student Award for Innovation, along with
+            fellow students Dawn Jordan and Kyle Moroscher. Parker was
+            recognized for his leadership in developing a smart assistant
+            swimming coach, creating a bowling alley data capture device, and
+            building an AI-driven robot. As vice president of the Shawnee State
+            IEEE chapter, Parker promotes collaboration and innovation within
+            the student community, fostering technical and professional growth.
+          </ArticleSummary>
+        </Section>
+        <Section label="Athletic Articles" bottomBorder>
+          <ArticleSummary
+            src="/article-thumbnails/csc-academic-all-district-2024.jpeg"
+            articleTitle="Clay, Lauders named as CSC Academic All-District At-Large honorees for SSU men's bowling"
+            articleURL="https://www.ssubears.com/sports/mbowl/2023-24/releases/2024062433abps"
+            articleAuthor="Kevin Colley"
+            articleSite="Shawnee State Bears"
+          >
+            Parker Lauders and Sam Clay, standout athletes from Shawnee State's
+            men's bowling team, were named to the College Sports Communicators'
+            (CSC) Academic All-District At-Large Team. Lauders, a sophomore
+            majoring in computer engineering technology with a 3.85 GPA, played
+            a key role in SSU's first bowling event victory and earned the 2024
+            Presidential Award for Innovation. Clay, an exercise science major
+            with a 3.73 GPA, led the team with consistent top performances,
+            including a second-place finish at the Greater Pittsburgh Collegiate
+            Classic. Both athletes exemplify excellence in academics and
+            athletics.
+          </ArticleSummary>
+          <ArticleSummary
+            src="/article-thumbnails/academic-all-obc.jpeg"
+            articleTitle="Nine named Academic All-OBC for men's bowling"
+            articleURL="https://www.ssubears.com/sports/mbowl/2023-24/releases/20240329kdj6o6"
+            articleAuthor="Kevin Colley"
+            articleSite="Shawnee State Bears"
+          >
+            Shawnee State University’s men's bowling program had nine athletes
+            named Academic All-OBC honorees, including Sam Clay, Parker Lauders,
+            and Ashton Heckman, who achieved a clean sweep of academic awards by
+            earning Academic All-OBC, NAIA Scholar-Athlete, and NCBCA honors.
+            Other honorees include Isaac Halter, Zach Ison, Blake Landers,
+            Brendan Major, Jakob Pelfrey, and Andrew Schoener. The recognition
+            highlights their academic excellence, with all maintaining GPAs
+            above 3.25 across various majors.
+          </ArticleSummary>
+          <ArticleSummary
+            src="/article-thumbnails/ncbca-scholar-athlete.jpeg"
+            articleTitle="Clay, Heckmn, Ison, Landers, Lauders, Schoener earn NCBCA Academic honors"
+            articleURL="https://www.ssubears.com/sports/mbowl/2023-24/releases/20240321ibykw3"
+            articleAuthor="Kevin Colley"
+            articleSite="Shawnee State Bears"
+          >
+            Six members of Shawnee State University's men's bowling team,
+            including Sam Clay, Ashton Heckman, Zach Ison, Blake Landers, Parker
+            Lauders, and Andrew Schoener, were named National Collegiate Bowling
+            Coaches Association (NCBCA) academic honorees. Clay, Heckman, and
+            Lauders also earned NAIA Scholar-Athlete recognition. Landers and
+            Ison hold the highest GPAs on the team, with 3.94 and 3.93,
+            respectively. Schoener rounds out the honorees with a 3.63 GPA. All
+            six bowlers will return next season as part of the 2024-25 squad.
+          </ArticleSummary>
+          <ArticleSummary
+            src="/article-thumbnails/naia-scholar-athletes.jpeg"
+            articleTitle="Clay, Heckman, Lauders named NAIA Scholar-Athletes"
+            articleURL="https://www.ssubears.com/sports/mbowl/2023-24/releases/20240320zqw1o9"
+            articleAuthor="Kevin Colley"
+            articleSite="Shawnee State Bears"
+          >
+            Shawnee State University's men's bowling team had three athletes,
+            Sam Clay, Ashton Heckman, and Parker Lauders, recognized as NAIA
+            Scholar-Athletes, the NAIA's highest academic honor. Heckman, a
+            graphic design major with a 3.83 GPA, Lauders, a computer
+            engineering technology major with a 3.79 GPA, and Clay, an exercise
+            science major with a 3.67 GPA, all boast impressive academic
+            achievements. The trio will return as part of the team that recently
+            qualified for the Intercollegiate Team Sectionals for the second
+            time in program history.
+          </ArticleSummary>
           <ArticleSummary
             src="/article-thumbnails/lauders-bowl.png"
             articleTitle="Lauders bowls his way to success at Shawnee State University"
